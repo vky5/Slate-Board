408c18f9ff6931b4cb35a4baa05d39f7c31cae27
vky5 <vky05@proton.me>
2024-03-15 22:22:46 +0530

    solved contribution issue

--- slateFrontend/src/components/Textar.jsx
+++ slateFrontend/src/components/Textar.jsx
@@ -3,7 +3,7 @@ import './style/textar.css'
 function Textar() {
   return (
     <div className='text'>
-      <input type="text" placeholder='Add heading here...' className='notes-heading' />
+      <input type="text" className='notes-heading' />
       <textarea name="notes" id="notes"></textarea>
     </div>
   )
