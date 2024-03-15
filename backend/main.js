956e52a042ea5d0206f5f87e0d4275d212a6405e
vky5 <beta@blacktree.comnect.local>
2024-03-15 18:04:40 +0530

    resolved cors issue

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
 
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PATCH,DELETE'
+}))
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.status(200).send("Hey Boii")
+})
 
 app.use('/api/v1/auth', userRoutes); 
 
